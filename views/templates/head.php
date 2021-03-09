1a329c92d32daa7cfb56c0c29ef62543a94b5ee9
jahir <jahano@outlook.com>
2021-03-09 14:09:10 -0600

    Se cambia la pagina a HTML, se agrega Smoth-Scroll (Avance 1 09/03/2021)

--- views/templates/head.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Noe Jahir Personal web<</title>
-    <link rel="stylesheet" href="../assets/css/style.css">
-    <link href="../assets/lightbox2-2.11.3/src/css/lightbox.css" rel="stylesheet" />
-</head>
