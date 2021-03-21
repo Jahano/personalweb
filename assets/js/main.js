0131c993785e50791edde93c20426187053780e0
jahir <jahano@outlook.com>
2021-03-20 19:02:45 -0600

    SE AGREGA CAMBIO DE SECTION CON JS; PENDIENTE ANIMACIONES

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,18 @@
-const barIcon = document.getElementById('iconbar');
-const headerNav = document.getElementById('nav-menu');
-let flag = false;
+const sec1 = document.getElementById('sec-1');
+const sec2 = document.getElementById('sec-2');
+const sec3 = document.getElementById('sec-3');
+/*NAV BAR ANCHORS */
+const nav1 = document.getElementById('nav-sec1');
+const nav2 = document.getElementById('nav-sec2');
+const nav3 = document.getElementById('nav-sec3');
+
+
+const title = document.getElementById('main__title');
+
 function showNav(){
+    const barIcon = document.getElementById('iconbar');
+    const headerNav = document.getElementById('nav-menu');
+    let flag = false;
     barIcon.addEventListener('click' , function(){
         if(!flag){
             flag = true;
@@ -29,4 +40,71 @@ function showNav(){
     });    
 }
 
-showNav();
+
+function showSect1(){
+        
+    nav1.addEventListener('click' , function(){
+        
+        title.innerHTML = "Experiencia";
+        sec1.style.display="flex";
+        sec2.style.display="none";
+        sec3.style.display="none";
+
+        
+       
+        
+    });
+}
+function showSect2(){
+    nav2.addEventListener('click' , function(){
+        
+        title.innerHTML = "Contacto";
+        
+        sec1.style.display="none";
+        sec2.style.display="flex";
+        sec3.style.display="none";
+       
+        
+    });
+}
+function showSect3(){
+    nav3.addEventListener('click' , function(){
+        
+        title.innerHTML = "FAQ";
+        sec1.style.display="none";
+        sec2.style.display="none";
+        sec3.style.display="flex";
+       
+    });
+}
+
+
+// function getSecOnDisplay(){
+//     let comp;
+//     if(getComputedStyle(sec1).display == "flex"){
+//         comp = 1;
+//         return comp;
+//     }
+//     else if(getComputedStyle(sec2).display == "flex"){
+//         comp = 2;
+//         return comp;
+//     }
+//     else{
+//         comp = 3;
+//         return comp;
+//     }
+// }
+
+
+const dovRead=function(){
+/*MAIN SECTIONS */
+    
+    
+    showNav();
+    showSect1();
+    showSect2();
+    showSect3();
+    
+}
+dovRead();
+
